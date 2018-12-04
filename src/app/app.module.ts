eecfb1130b2b0ce2f4fc467489ffd1feaab9a66b
Unni <unni@soupedup.com>
2018-12-04 12:38:49 +0530

    bootstrap, Fontawesome and basic sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,8 @@ import { HeaderComponent, FooterComponent, PublicComponent, SecureComponent } fr
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { DashboardComponent } from './components/user/dashboard/dashboard.component';
-
-
+import { AlertModule } from 'ngx-bootstrap';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +22,9 @@ import { DashboardComponent } from './components/user/dashboard/dashboard.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlertModule.forRoot(),
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
